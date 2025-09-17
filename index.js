17cc2574b914c47aeab7253998fb0452d774fec3
Appleeye0091 <167168466+Appleeye0091@users.noreply.github.com>
2025-09-17 17:43:32 +0530

    two_basic_express_app

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app= express();
 app.get('/' , (req, res) => {
     res.json({
         "type": "greeting",
-        "value": "congrats user!"
+        "value": "congrats sourav!"
     })
 })
 
